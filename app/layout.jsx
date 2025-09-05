b4ab9faa2b8d4ee8f43382b5f7ab723499216c3a
Ximena Girasol <girasolximena@gmail.com>
2025-09-05 20:17:21 +0000

    add inter font back

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,10 @@ import '../styles/globals.css';
 import { Footer } from '../components/footer';
 import { Header } from '../components/header';
 import localFont from 'next/font/local';
+import {Inter} from 'next/font/google';
+
+// Example of using a Google font with next/font
+const interFont = Inter({ subsets: ['latin'] });
 
 // Example of using a local font with next/font
 const sawesomeFont = localFont({ src: '../styles/sawesome.woff2', display: 'swap', variable: '--font-sawesome' });
@@ -19,7 +23,7 @@ export default function RootLayout({ children }) {
             <head>
                 <link rel="icon" href="/favicon.svg" sizes="any" />
             </head>
-            <body className={`antialiased text-white bg-red-900 ${sawesomeFont.variable}`} >
+            <body className={`antialiased text-white bg-red-900 ${sawesomeFont.variable} ${interFont.className}`} >
                 <div className="flex flex-col min-h-screen px-6 bg-noise sm:px-12">
                     <div className="flex flex-col w-full max-w-5xl mx-auto grow">
                         <Header />
